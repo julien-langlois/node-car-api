1491ce42deda0898681e6faea3091b9fd0e3adfe
Yassine AZZOUT <yassine@synthesio.com>
2018-04-15 20:44:24 +0200

    feat(description): add the tc_vars description for each model

--- src/api.js
+++ src/api.js
@@ -171,3 +171,22 @@ module.exports.search = async (payload, configuration = {}) => {
     return Promise.reject(e);
   }
 };
+
+/**
+ * Find a variable into script
+ * @param  {String} variable
+ * @param  {String} script
+ * @return {String}
+ */
+module.exports.findVariable = (variable, script) => {
+  const content = script.trim().replace(/&#{0,1}[a-z0-9]+;/ig, '');
+  const matches = content.substring(content.search(variable.replace('=', '')) + variable.length, content.length);
+  const clean = matches.replace('=', '');
+  const semicolon = clean.search(';');
+
+  if (semicolon > 0) {
+    return clean.substring(0, semicolon);
+  }
+
+  return clean;
+};
--- src/get-models.js
+++ src/get-models.js
@@ -1,8 +1,50 @@
 const cheerio = require('cheerio');
-const {get} = require('./api');
+const {get, findVariable} = require('./api');
 const getRecords = require('../src/get-records');
 const pSettle = require('p-settle');
 const uuidv5 = require('uuid/v5');
+
+/**
+ * Get the tc_vars
+ * @param  {String} body
+ * @return {Object}
+ */
+ /*eslint-disable camelcase*/
+const getTcVars = body => {
+  const $ = cheerio.load(body, {
+    'normalizeWhitespace': false,
+    'xmlMode': false,
+    'decodeEntities': true
+  });
+  let tc_vars = null;
+
+  $('script').each(function each () {
+    const text = $(this).html().toString();
+
+    if (text.includes('tc_vars')) {
+      try {
+        // the json is not valid, we have to
+        //1. replace all ' with "
+        //2 .add "" for some values
+        //3. remove last comma
+        const item = findVariable('var tc_vars =', text)
+          .replace(/'/g, '"')
+          .replace(/: ([^"]*),/g, ':\"$1\",')
+          .replace(/,(?=[^,]*$)/, '');
+
+        tc_vars = JSON.parse(item);
+
+        return;
+      } catch (e) {
+        console.error(e);
+        return;
+      }
+    }
+  });
+
+  return tc_vars;
+};
+/*eslint-enable camelcase*/
 
 /**
  * Get required specification
@@ -20,9 +62,11 @@ const getSpec = async (brand, record, configuration) => {
     const model = $('.ttlNav a > span').text();
     const [volume] = $('.caract02').text().split('/').map(item => item.replace(/\D/g, ''));
     const image = $('.ficheTech img.img-responsive').attr('src');
+    const description = getTcVars(response.text);
 
     return {
       brand,
+      description,
       image,
       model,
       volume,
